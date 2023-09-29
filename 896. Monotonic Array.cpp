4d6408fed3fbd580da732efb8f1150fa33fbc290
Shafi Abdullah <104518333+C-loud-Nine@users.noreply.github.com>
2023-09-29 10:24:06 +0600

    Create 896. Monotonic Array.cpp

---
+++ 896. Monotonic Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int len=nums.size();
+
+        int flag1=0,flag2=0;
+
+        for(int i=1;i<len;i++){
+            if(nums[i]<nums[i-1]){
+                flag1=1;
+            }
+            else if(nums[i]>nums[i-1]){
+                flag2=1;
+            }
+        }
+
+        if(flag1&&flag2) return false;
+        return true;
+        
+    }
+};
